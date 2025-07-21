02e3a5adcc690e6c8cfb500f44c6cd1eacf374a0
Mohammad-Ahrari <mahrari504@gmail.com>
2025-07-21 19:35:41 +0330

    feat(chat): add ChatHeader and setup basic ChatContainer structure

---
+++ frontend/src/components/ChatContainer.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+import MessageInput from "./MessageInput";
+import ChatHeader from "./ChatHeader";
+
+const ChatContainer = () => {
+  const { messages, getMessages, isMessageLoading, selectedUser } =
+    useChatStore();
+
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
+
+  if (isMessageLoading) return <div>Loading...</div>;
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+
+      <p>messages...</p>
+
+      <MessageInput />
+    </div>
+  );
+};
+
+export default ChatContainer;
